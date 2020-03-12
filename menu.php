e57ac2ab2da9f8749988d6963091af944844fcd3
karlarob <riie.muffin@gmail.com>
2020-03-12 00:15:50 -0500

    Archivos

---
+++ menu.php
@@ -0,0 +1,19 @@
+
+<div id="smallMenu" class="backColor1 alignTop">
+  <div onclick="openMenu();" class="svgHolder"><img class="svg svgColor2" src="<?php bloginfo('template_directory'); ?>/assets/images/svgs/menu.svg"></div>
+</div>
+<div id="menuShadow" class="shadow fullWidth fullHeight"></div>
+<div id="menu" class="backColor2 relative" >
+  <div  class="titleHolder backColor1">
+    <div onclick="closeMenu();" class="svgHolder"><img class="svg svgColor2" src="<?php bloginfo('template_directory'); ?>/assets/images/svgs/cross.svg"></div>
+  </div>
+  <a href="/cuenta"><p class="option color1">Mi Cuenta</p></a>
+  <a href="/cotizaciones"><p class="option color1">Cotizaciones</p></a>
+  <a href="/productos"><p class="option color1">Productos</p></a>
+  <a href="/clientes"><p class="option color1">Clientes</p></a>
+  <a href="/estadisticas"><p class="option color1">Estadisticas</p></a>
+  <a href="/"><p class="option color1">Inicio</p></a>
+  <a href="/wp-login.php?action=register"><p class="option color1">Registro</p></a>
+  <a href="/contacto"><p class="option color1">Contacto</p></a>
+  <div class="division backColor1 "></div>
+</div>
